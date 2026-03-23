203b3c5f53b22796677e79a34088dc3027e08f6d
PJB <qkrwoqud1234@naver.com>
2026-03-23 14:15:08 +0900

    87

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags  
+ * 
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+
+protected:
+	
+
+private:
+	static FAuraGameplayTags GameplayTags;
+
+};
