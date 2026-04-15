81459ed57d8caa743bcee206a284bcfd77d2aa07
PJB <qkrwoqud1234@naver.com>
2026-04-16 00:42:42 +0900

    143

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -46,7 +46,7 @@ void AAuraProjectile::BeginPlay()
 
 void AAuraProjectile::Destroyed()
 {
-	//AB_LOG_NET_INFO_ACTOR(this);
+	AB_LOG_NET_INFO_ACTOR(this);
 
 	if (!bHit && !HasAuthority())
 	{
@@ -61,7 +61,7 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 
-	//AB_LOG_NET_INFO_ACTOR(this);
+
 
 	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
