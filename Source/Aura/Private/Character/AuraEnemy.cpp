c2077180a572f453158eeb314894b240609a0235
PJB <qkrwoqud1234@naver.com>
2026-05-10 17:08:10 +0900

    179

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -68,6 +68,16 @@ void AAuraEnemy::UnHighlightActor()
 	
 }
 
+void AAuraEnemy::SetCombatTarget_Implementation(AActor* InCombatTarget)
+{
+	CombatTarget = InCombatTarget;
+}
+
+AActor* AAuraEnemy::GetCombatTarget_Implementation() const
+{
+	return CombatTarget;
+}
+
 int32 AAuraEnemy::GetPlayerLevel()
 {
 	return Level;
