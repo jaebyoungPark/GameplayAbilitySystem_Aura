22e28aaa3bf5fb70b303c3ffb9839e11a83a890a
PJB <qkrwoqud1234@naver.com>
2026-05-26 14:27:20 +0900

    225

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -31,9 +31,11 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void Destroyed() override;
 
-
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> Sphere;
 
 private:
 	UPROPERTY(EditDefaultsOnly)
@@ -41,8 +43,6 @@ private:
 
 	bool bHit = false;
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
